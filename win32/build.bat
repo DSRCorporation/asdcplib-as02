476153879789e2d3af47ca47d7c1d6afe81457e1
Alexandr <Alexandr>
2016-07-06 15:01:21 +0300

    Automated build and binaries copying added

--- win32/build.bat
+++ win32/build.bat
@@ -1,1 +1,17 @@
 nmake WITH_OPENSSL=../proj/vendors/openssl WITH_XERCES=../proj/vendors/xerces USE_AS_02=1 /f Makefile.mak
+
+IF ERRORLEVEL 1 GOTO ERROR 
+
+move /Y "as-02-unwrap.exe" "..\bin"
+copy /Y "..\proj\vendors\openssl\bin\*.dll"  "..\bin"
+copy /Y "..\proj\vendors\xerces\bin\*.dll"   "..\bin"
+
+echo "All must be in ../bin folder."
+goto END
+
+
+:ERROR
+echo "nmake build failed."
+
+
+:END
